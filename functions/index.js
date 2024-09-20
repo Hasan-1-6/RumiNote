df8401b135208997302e7a82a6d25a37f9dc15b6
Hasan-1-6 <hasandilshad16@gmail.com>
2024-09-20 16:49:13 +0530

    Added data cleanup every 24 hours

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,31 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {onSchedule} = require("firebase-functions/v2/scheduler")
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+const db = admin.firestore()
+
+exports.deleteOldData = onSchedule("every 24 hours", async () =>{
+    const now = admin.firestore.Timestamp.now();
+
+    const twentyFourHoursAgo = new Date(now.toDate().getTime() - 24*60*60*1000);
+
+    const snapshot = await db.collection("diary").where('timestamp', '<', admin.firestore.Timestamp.fromDate(twentyFourHoursAgo)).get();
+
+    if(!snapshot.empty)
+    {
+        const batch = db.batch()
+        snapshot.forEach((doc) =>{
+            batch.delete(doc.ref);
+        });
+        await batch.commit();
+        console.log(`Deleted ${snapshot.size} succesfully`)
+    }
+
+
+})
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,44 @@ import Homepage from './components/Homepage.jsx'
 import { useState, useEffect } from 'react'
 import { auth } from './config/firebase.js'
 import Landing from './components/Landing.jsx'
+import { getFirestore, collection, query, where, getDocs, writeBatch, Timestamp, getDoc } from 'firebase/firestore'
+import { db } from './config/firebase.js'
+
+const deleteOldData = async () => {
+  try{
+    const now = Timestamp.now()
+    const twentyFourHoursAgo = new Date(now.toDate().getTime() - 24*60*60*1000);
+    const q = query(collection(db, 'diary'), where('timestamp', '<', Timestamp.fromDate(twentyFourHoursAgo)));
+    const querySnapshot = await getDocs(q)
+
+    if(!querySnapshot.empty) {
+      const batch = writeBatch(db)
+      querySnapshot.forEach((doc) => {
+          batch.delete(doc.ref)
+      });
+
+      await batch.commit();
+      console.log(`Deleted ${querySnapshot.size} succesfully`)
+    }
+    else {
+      console.log('No old data found (24hours)')
+    }
+    
+  }
+  catch (error) {
+    console.error("Error deleting old data: ", error);
+  }
+}
+
+
 
 function App() {
+
+
+  useEffect(() => {
+    deleteOldData();  // Run the deletion when the app loads
+  }, [])
+  
     const [user, setUser] = useState(null);
     
     const authchange = () => {
